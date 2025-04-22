b00518ab53d0255b9703fc7e4535230b5a332249
Eltern Held <elternheld@gmail.com>
2025-04-22 16:51:24 +0000

    Add new Section for Navigation "Use Case 1 - Ideenfinder Aktivitäten

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -119,6 +119,25 @@ export default function RootLayout({
                           </Link>
                         </SidebarMenuButton>
                       </SidebarMenuItem>
+                       <SidebarMenuItem>
+                        <SidebarMenuButton asChild>
+                        <Link href="/use-case-1" className="flex items-center">
+                           <Activity
+                              xmlns="http://www.w3.org/2000/svg"
+                              width={24}
+                              height={24}
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              stroke="currentColor"
+                              strokeWidth={2}
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              className="lucide lucide-activity mr-2 h-4 w-4"
+                            />
+                            <span>Use Case 1 - Ideenfinder Aktivitäten</span>
+                          </Link>
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
                       {userRole === 'admin' && (
                           <SidebarMenuItem>
                             <SidebarMenuButton asChild>
