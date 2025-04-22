f4f05c505088c6a610bae366e9b90e71e5d49bb4
Eltern Held <elternheld@gmail.com>
2025-04-22 16:55:05 +0000

    Landing Page für use case 1 erstellen

---
+++ src/app/use-case-1/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import VoiceToTextBrainstorming from "@/components/VoiceToTextBrainstorming";
+import React from "react";
+
+const UseCase1Page = () => {
+  return (
+    <div className="container py-12">
+      <VoiceToTextBrainstorming />
+    </div>
+  );
+};
+
+export default UseCase1Page;
