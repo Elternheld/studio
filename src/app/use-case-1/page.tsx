cd121525b27e9ee6623ee7922bf10dcdedf2b7c5
Eltern Held <elternheld@gmail.com>
2025-04-23 14:35:16 +0000

    create an own space chip like for "Konfiguration"

--- src/app/use-case-1/page.tsx
+++ src/app/use-case-1/page.tsx
@@ -4,13 +4,31 @@ import React from "react";
 import ActivityKonfigurator from "@/components/ActivityKonfigurator";
 import ActivitySuggestions from "@/components/ActivitySuggestions";
 import VoiceToTextBrainstorming from "@/components/VoiceToTextBrainstorming";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 
 const UseCase1Page = () => {
     return (
         <div className="container py-12">
-            <ActivityKonfigurator />
-            <VoiceToTextBrainstorming/>
-            <ActivitySuggestions />
+            <Card className="w-[80%] mx-auto">
+                <CardHeader>
+                    <CardTitle>Konfigurator</CardTitle>
+                    <CardDescription>Konfiguriere die Parameter für die Aktivitätsvorschläge.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <ActivityKonfigurator/>
+                </CardContent>
+            </Card>
+
+            <Card className="w-[80%] mx-auto mt-8">
+                <CardHeader>
+                    <CardTitle>Brainstorming - ElternHeld Voice Bot</CardTitle>
+                    <CardDescription>Brainstorme neue Ideen mit unserem Voice Bot.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <VoiceToTextBrainstorming/>
+                </CardContent>
+            </Card>
+            <ActivitySuggestions/>
         </div>
     );
 };
